e0190cd6cac5054be8ca241cdb8fd2f9ea454849
Michael Nadel <michael.nadel@gmail.com>
2015-10-20 19:50:27 -0500

    added select statement parser

---
+++ parser.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type Parser struct {
+	s   *Scanner
+	buf struct {
+		tok Token
+		lit string
+		n   int
+	}
+}
+
+type SelectStatement struct {
+	Fields     []string
+	BucketPath []string
+}
+
+func NewParser(r io.Reader) *Parser {
+	return &Parser{s: NewScanner(r)}
+}
+
+func (p *Parser) ParseSelect() (*SelectStatement, error) {
+	stmt := &SelectStatement{}
+
+	if tok, lit := p.scanNextNonWhitespaceToken(); tok != SELECT {
+		return nil, fmt.Errorf("found %q, expected %s", lit, SELECT)
+	}
+
+	for {
+		tok, lit := p.scanNextNonWhitespaceToken()
+		if tok != IDENT && tok != ASTERISK {
+			return nil, fmt.Errorf("found %q, expected field name or %s", lit, ASTERISK)
+		}
+		stmt.Fields = append(stmt.Fields, lit)
+
+		if tok, _ := p.scanNextNonWhitespaceToken(); tok != COMMA {
+			p.unscan()
+			break
+		}
+	}
+
+	if tok, lit := p.scanNextNonWhitespaceToken(); tok != FROM {
+		return nil, fmt.Errorf("found %q, expected %s", lit, FROM)
+	}
+
+	for {
+		tok, lit := p.scanNextNonWhitespaceToken()
+		if tok != IDENT && tok != BUCKETSEP {
+			return nil, fmt.Errorf("found %q, expected bucket name or %q", lit, BUCKETSEP)
+		}
+		stmt.BucketPath = append(stmt.BucketPath, lit)
+
+		if tok, _ := p.scanNextNonWhitespaceToken(); tok != BUCKETSEP {
+			p.unscan()
+			break
+		}
+	}
+
+	return stmt, nil
+}
+
+func (p *Parser) scanNextNonWhitespaceToken() (tok Token, lit string) {
+	tok, lit = p.scan()
+	if tok == WS {
+		tok, lit = p.scan()
+	}
+	return
+}
+
+func (p *Parser) scan() (tok Token, lit string) {
+	if p.buf.n != 0 {
+		p.buf.n = 0
+		return p.buf.tok, p.buf.lit
+	}
+
+	tok, lit = p.s.Scan()
+
+	p.buf.tok, p.buf.lit = tok, lit
+
+	return
+}
+
+func (p *Parser) unscan() {
+	p.buf.n = 1
+}
---
+++ parser_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCanParseSimpleSelect(t *testing.T) {
+	p := NewParser(strings.NewReader("select a from b"))
+
+	parsed, err := p.ParseSelect()
+	assert.NoError(t, err, "error parsing")
+
+	assert.Equal(t, 1, len(parsed.Fields))
+	field := parsed.Fields[0]
+	assert.Equal(t, "a", field)
+
+	assert.Equal(t, 1, len(parsed.BucketPath))
+	bucket := parsed.BucketPath[0]
+	assert.Equal(t, "b", bucket)
+}
+
+func TestCanParseMultipleFields(t *testing.T) {
+	p := NewParser(strings.NewReader("select a,b from c"))
+
+	parsed, err := p.ParseSelect()
+	assert.NoError(t, err, "error parsing")
+
+	assert.Equal(t, 2, len(parsed.Fields))
+	field := parsed.Fields[0]
+	assert.Equal(t, "a", field)
+	field = parsed.Fields[1]
+	assert.Equal(t, "b", field)
+}
+
+func TestCanParseMultipleBuckets(t *testing.T) {
+	p := NewParser(strings.NewReader("select a,b from b1/b2/c"))
+
+	parsed, err := p.ParseSelect()
+	assert.NoError(t, err, "error parsing")
+
+	assert.Equal(t, 3, len(parsed.BucketPath))
+	bucket := parsed.BucketPath[0]
+	assert.Equal(t, "b1", bucket)
+	bucket = parsed.BucketPath[1]
+	assert.Equal(t, "b2", bucket)
+	bucket = parsed.BucketPath[2]
+	assert.Equal(t, "c", bucket)
+}
